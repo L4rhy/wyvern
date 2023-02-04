718d123440c7a00c81488971460bddb6ea4681f5
L4rhy <lara.vsteinbach@gmail.com>
2023-02-04 11:48:38 -0300

    deploy 3

--- src/pages/components/usuario.tsx
+++ src/pages/components/usuario.tsx
@@ -1,18 +1,29 @@
 import { Caixa, CaixaUsuario, NomeUsuario, BotaoLogout, CaixaAcoes, Texto, Botao } from "@/styles/components/styleUsuario"
 import { RootState } from "@/stack"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { useRouter } from "next/router"
+import { logout } from "@/stack/stock"
 
 export default function Usuario(){
     const stock = useSelector((state:RootState)=>state.stock)
+    const router = useRouter()
+    const dispatch = useDispatch()
+    const VoltaUsuario = () =>{
+        router.push("/usuario")
+    }
+    const Logout = () =>{
+        dispatch(logout())
+        router.push("/login")
+    }
     return(
         <Caixa>
             <CaixaUsuario>
                 <NomeUsuario>{stock.usuario.nomeUsuario}</NomeUsuario>
-                <BotaoLogout href="/">Logout</BotaoLogout>
+                <BotaoLogout onClick={()=>Logout}>Logout</BotaoLogout>
             </CaixaUsuario>
             <CaixaAcoes>
                 <Texto>Voltar a <br/>Pagina de Usuario</Texto>
-                <Botao href="/usuario">Voltar</Botao>
+                <Botao onClick={()=>VoltaUsuario}>Voltar</Botao>
             </CaixaAcoes>
         </Caixa>
     )
